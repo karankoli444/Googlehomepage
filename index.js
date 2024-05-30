429c89dc7001755f63c2eda343fb3f889772e59e
karan koli <146938961+karankoli444@users.noreply.github.com>
2024-05-30 13:53:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+function toggledashbaord() {
+    let dashboard = document.querySelector('.apps-bar');
+    dashboard.classList.toggle('toogle')
+}
+
+function textclear() {
+    document.querySelector('#input').value = ''
+}
+
+
+
